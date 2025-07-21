3724d4a411dfbf6b984781f9537aeffee324d07a
Maksym <max669@ukr.net>
2025-07-21 11:12:27 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,14 +1,25 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", handleSubmit);
 
-const registered = document.querySelector(".login-form");
-registered.addEventListener("submit", handleSubmit);
 function handleSubmit(event) {
-    event.preventDefault();
-    const form = event.target;
-    const login = form.elements.email.value.trim();
-    const password = form.elements.password.value.trim();
-    if (login === "" || password === "") {
-        return console.log("All form fields must be filled in");
-    }
-    console.log({login, password});
-    form.reset();
+  event.preventDefault();
+ 
+  const elements = event.target.elements;
+
+
+  const email = elements.email.value.trim();
+  const password = elements.password.value.trim();
+  
+  if(email === "" || password === "") {
+    return alert("All form fields must be filled in");
+  }
+
+  const info = {
+    email,
+    password
+  }
+
+  console.log(info);
+
+  event.target.reset();
 }
